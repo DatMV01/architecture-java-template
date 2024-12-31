5997a48ee66b4fabad75893531643eb6fe8e362b
DatMV <145959805+DatMV01@users.noreply.github.com>
2024-12-31 18:41:32 +0700

    Initial clean-architecture-module

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# architecture-java-template
+<h1> architecture-java-template </h1>
+<h2> clean-architecture-module </h2>
+
+![clean-architecture-module.png](clean-architecture-module.png)
---
+++ application/src/test/java/com/example/architecture/application/AppTest.java
@@ -0,0 +1,12 @@
+package com.example.architecture.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    void addition() {
+        assertEquals(2, 2);
+    }
+}
